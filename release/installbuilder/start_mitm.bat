a0c67668fd91ca2ae1c271764ab35dd1ad50319c
Sujatha Shekar Shetty <sujatha.sumanth@gmail.com>
2024-11-13 22:12:08 -0800

    Update start_mitm.bat

--- release/installbuilder/start_mitm.bat
+++ release/installbuilder/start_mitm.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0\bin\run.ps1" mitmdump -p 8080 --set block_global=false
+powershell.exe -ExecutionPolicy Bypass -File "%~dp0\run.ps1" mitmdump -p 8080 --set block_global=false
