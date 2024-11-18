a9015d00802997d922570fe126bd85f5024a2101
doublependulum <sumanthcoder@gmail.com>
2024-11-17 20:42:57 -0800

    Update run.ps1 to take command arguments

--- release/installbuilder/run.ps1
+++ release/installbuilder/run.ps1
@@ -1,6 +1,9 @@
 $tool = $args[0]
+$toolargs = $args[1]
+Write-Host "tool: $tool"
+Write-Host "toolargs: $toolargs"
 if (Get-Command wt -ErrorAction SilentlyContinue) {
-	Start-Process wt -ArgumentList "powershell.exe","-Command","& '$PSScriptRoot\$tool.exe'"
+	Start-Process wt -ArgumentList "powershell.exe -ExecutionPolicy Bypass","-Command","& $PSScriptRoot\$tool.exe $toolargs"
 } else { 
-	Start-Process powershell -ArgumentList "-Command","& '$PSScriptRoot\$tool.exe'"
+	Start-Process powershell -ExecutionPolicy Bypass -ArgumentList "-Command","& $PSScriptRoot\$tool.exe $toolargs"
 }
